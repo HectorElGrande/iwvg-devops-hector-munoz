24b752a25c2f7347c8e7aaa7f76bbacef05065b7
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2020-09-20 11:41:38 +0200

    Searches and SearchesTest added the findDecimalFractionByUserName's method #11

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
@@ -4,14 +4,23 @@ import es.upm.miw.iwvg_devops.code.Fraction;
 import es.upm.miw.iwvg_devops.code.Searches;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SearchesTest {
 
     @Test
-    void findHighestFractionTest() {
+    void testFindHighestFraction() {
         Fraction value = new Searches().findHighestFraction();
         assertEquals("2/1", value.getNumerator() + "/" + value.getDenominator());
+    }
+
+    @Test
+    void testFindDecimalFractionByUserName(){
+        assertEquals(List.of(0.0, 1.0, 2.0, 0.2, -0.5, 0.5, 1.0), new Searches().findDecimalFractionByUserName("Oscar")
+                .collect(Collectors.toList()));
     }
 
 }
